82b7df943359d8a136da514573d510c99fa3b0ed
Raghav Chugh <raghavchughcode@gmail.com>
2023-09-14 10:18:23 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Element(){
+function App(){
     const [showP, setShowP] = React.useState(false);
 
     React.useEffect(()=>{setTimeout(() => { setShowP(true); }, 5000)}, []);
@@ -10,5 +10,6 @@ function Element(){
                 {showP && <p>yoyoyo</p>}
             </div>);
 }
-const root = ReactDOM.hydratRoot(document.getElementById("root"));
-root.render(<Element/>);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
