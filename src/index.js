9972d2150f36d1b6bbb9420a04fac0d116292142
Raghav Chugh <raghavchughcode@gmail.com>
2023-09-14 10:04:07 +0530

    setting up, better

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("aur kya haal chaal?");
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function Element(){
+    const [showP, setShowP] = React.useState(false);
+
+    React.useEffect(()=>{setTimeout(() => { setShowP(true); }, 5000)}, []);
+    return (<div>
+                <h1>Hello World</h1>
+                {showP && <p>yoyoyo</p>}
+            </div>);
+}
+const root = ReactDOM.hydratRoot(document.getElementById("root"));
+root.render(<Element/>);
